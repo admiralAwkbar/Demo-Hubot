5e4989ca043e12985021e1ab6aa96bde9480b77f
Lukas Gravley <admiralawkbar@github.com>
2018-12-05 06:49:59 -0800

    fix consul reference

--- Automation/updateGitHubStatus.pl
+++ Automation/updateGitHubStatus.pl
@@ -43,62 +43,72 @@ Footer();               # Basic footer prints
 #### SUB ROUTINE Header ###############################################
 sub Header
 {
-  	print "-------------------------------------------------------------------\n";
-  	print "This step will send status back to GitHub\n";
-   print "-------------------------------------------------------------------\n";
-   print "The Org:\[$org\]\n";
-   print "The Repo:\[$repo\]\n";
-   print "The Branch:\[$branch\]\n";
-   print "The Sha:\[$sha\]\n";
-   print "The Status:\[$preflightStatus\]\n";
-   print "The TargetUrl:\[$targetUrl\]\n";
-  	print "-------------------------------------------------------------------\n";
+  print "-------------------------------------------------------------------\n";
+  print "This step will send status back to GitHub\n";
+  print "-------------------------------------------------------------------\n";
+  print "The Org:\[$org\]\n";
+  print "The Repo:\[$repo\]\n";
+  print "The Branch:\[$branch\]\n";
+  print "The Sha:\[$sha\]\n";
+  print "The Status:\[$preflightStatus\]\n";
+  print "The TargetUrl:\[$targetUrl\]\n";
+  print "-------------------------------------------------------------------\n";
 }
 #######################################################################
 #### SUB ROUTINE Footer ###############################################
 sub Footer
 {
-  	print "-------------------------------------------------------------------\n";
-    print "Step has completed with $state\n";
-    exit($exitCode);
+  print "-------------------------------------------------------------------\n";
+  print "Step has completed with $state\n";
+  exit($exitCode);
 }
 #######################################################################
 #### SUB ROUTINE ValidateInput ########################################
 sub ValidateInput
 {
-    # need to have a SHA to update GHE
-    if ($sha =~ m/none/i)
-    {
-        print "We never recieved a SHA!\n";
-        print "Cannot send update\n";
-        exit(1);
-    }
+  # need to have a SHA to update GHE
+  if ($sha =~ m/none/i)
+  {
+    print "We never recieved a SHA!\n";
+    print "Cannot send update\n";
+    exit(1);
+  }
 
-    if (length($githubToken) < 1)
-    {
-      # Failed to get key from env, need to pull it
-      my $command = "grep GITHUB_TOKEN /opt/hubot/hubot.env 2>&1";
-      my $result = `$command`;
-
-      if ($?!=0)
-      {
-         print "ERROR! failed to get Github Token!\n";
-         exit(1);
-      }
-      else
-      {
-         chomp($result);
-         my ($var1,$var2)=split(" ",$result,2);
-         my ($trash1,$token,$trash2)=split("'",$var2,3);
-         $githubToken = $token;
-      }
-    }
-
-    ###################
-    # Get the GHE Url #
-    ###################
-    my $command = "getent hosts github.service.consul | awk \'{ print \$1 }\' 2>&1";
+  if (length($githubToken) < 1)
+  {
+    # Failed to get key from env, need to pull it
+    my $command = "grep GITHUB_TOKEN /opt/hubot/hubot.env 2>&1";
     my $result = `$command`;
+
+    if ($?!=0)
+    {
+      print "ERROR! failed to get Github Token!\n";
+      exit(1);
+    }
+    else
+    {
+      chomp($result);
+      my ($var1,$var2)=split(" ",$result,2);
+      my ($trash1,$token,$trash2)=split("'",$var2,3);
+      $githubToken = $token;
+    }
+  }
+
+  ###################
+  # Get the GHE Url #
+  ###################
+  $gitHubUrl = "https://github.service.consul/api/v3";  # GHE Url
+  $statusUrlBase = "$gitHubUrl/repos/$org/$repo/statuses";# Url for Status
+
+  #######################
+  # Fix the Jenkins Url #
+  #######################
+  # TargetUrl example: http://jenkins.service.consul:8080/job/Hubot-Preflight/4/
+  if ($targetUrl =~ "jenkins.service.consul")
+  {
+    # Need to get the real Jenkins URL
+    my $getJenkinsCommand = "getent hosts jenkins.service.consul | awk \'{ print \$1 }\' 2>&1";
+    my $jenkinsResult = `$getJenkinsCommand`;
 
     # Make sure we have a valid result
     if ($?!=0)
@@ -108,104 +118,80 @@ sub ValidateInput
     }
     else
     {
-      chomp($result); # clean whitespace
-      $gitHubUrl = "https://$result/api/v3";  # GHE Url
-      $statusUrlBase = "$gitHubUrl/repos/$org/$repo/statuses";# Url for Status
+      chomp($jenkinsResult); # clean whitespace
+      my ($var1,$var2,$var3,$var4,$var5,$var6)=split(/\//,$targetUrl,6);
+      $targetUrl = "http://$jenkinsResult:8080/job/$var5/$var6";
     }
-
-    #######################
-    # Fix the Jenkins Url #
-    #######################
-    # TargetUrl example: http://jenkins.service.consul:8080/job/Hubot-Preflight/4/
-    if ($targetUrl =~ "jenkins.service.consul")
-    {
-      # Need to get the real Jenkins URL
-      my $getJenkinsCommand = "getent hosts jenkins.service.consul | awk \'{ print \$1 }\' 2>&1";
-      my $jenkinsResult = `$getJenkinsCommand`;
-
-      # Make sure we have a valid result
-      if ($?!=0)
-      {
-        print "ERROR! Failed to resolve Github URL!\n";
-        exit(1);
-      }
-      else
-      {
-        chomp($jenkinsResult); # clean whitespace
-        my ($var1,$var2,$var3,$var4,$var5,$var6)=split(/\//,$targetUrl,6);
-        $targetUrl = "http://$jenkinsResult:8080/job/$var5/$var6";
-      }
-    }
+  }
 }
 #######################################################################
 #### SUB ROUTINE ReportStatus ########################################
 sub ReportStatus
 {
-    #############################
-    # Convert string to boolean #
-    #############################
-    if ($preflightStatus =~ m/success/i)
+  #############################
+  # Convert string to boolean #
+  #############################
+  if ($preflightStatus =~ m/success/i)
+  {
+    $exitCode = 0;
+    $state = "success";
+  }
+  else
+  {
+    $exitCode = 1;
+    $state = "failure";
+  }
+
+  #######################
+  # Set the Description #
+  #######################
+  $description = "The build was a $state";
+
+  print "-------------------------------------------------------------------\n";
+
+  if (length($branch) or length($sha))
+  {
+    #################
+    # Set StatusUrl #
+    #################
+    # need to append the commit hash
+    my $statusUrl .= "$statusUrlBase/$sha";
+    #print "DEBUG --- Status URL:\[$statusUrl\]\n";
+
+    # Need to end api call to GHE for Success or failure
+    my $command = "curl -s -k -X POST -H \'Authorization: token $githubToken\' -H \'Accept: application/vnd.github.v3+json\' -d \'{\"state\":\"$state\", \"target_url\":\"$targetUrl\", \"description\":\"$description\", \"context\":\"$context\"}\' $statusUrl 2>&1";
+    my $cleanCommand = "curl -s -k -X POST -H \'Authorization: token XXXXXXXX\' -H \'Accept: application/vnd.github.v3+json\' -d \'{\"state\":\"$state\", \"target_url\":\"$targetUrl\", \"description\":\"$description\", \"context\":\"$context\"}\' $statusUrl 2>&1";
+
+    print "Sending curl to GHE:\[$cleanCommand\]\n";
+    my @results = `$command`;
+
+    # Check the return from GHE
+    if ($?!=0)
     {
-        $exitCode = 0;
-        $state = "success";
+      foreach my $result (@results)
+      {
+        print "ERROR! FAILED TO SEND DATA\n";
+        chomp($result);
+        print "Result:\[$result\]\n";
+      }
+      DebugErrors();
     }
     else
     {
-        $exitCode = 1;
-        $state = "failure";
+      print "Successfully sent status:\[$state\] for branch:\[$branch\]\n";
     }
-
-    #######################
-    # Set the Description #
-    #######################
-    $description = "The build was a $state";
-
-    print "-------------------------------------------------------------------\n";
-
-    if (length($branch) or length($sha))
-    {
-        #################
-        # Set StatusUrl #
-        #################
-        # need to append the commit hash
-        my $statusUrl .= "$statusUrlBase/$sha";
-        #print "DEBUG --- Status URL:\[$statusUrl\]\n";
-
-        # Need to end api call to GHE for Success or failure
-        my $command = "curl -s -k -X POST -H \'Authorization: token $githubToken\' -H \'Accept: application/vnd.github.v3+json\' -d \'{\"state\":\"$state\", \"target_url\":\"$targetUrl\", \"description\":\"$description\", \"context\":\"$context\"}\' $statusUrl 2>&1";
-        my $cleanCommand = "curl -s -k -X POST -H \'Authorization: token XXXXXXXX\' -H \'Accept: application/vnd.github.v3+json\' -d \'{\"state\":\"$state\", \"target_url\":\"$targetUrl\", \"description\":\"$description\", \"context\":\"$context\"}\' $statusUrl 2>&1";
-
-        print "Sending curl to GHE:\[$cleanCommand\]\n";
-        my @results = `$command`;
-
-        # Check the return from GHE
-        if ($?!=0)
-        {
-            foreach my $result (@results)
-            {
-                print "ERROR! FAILED TO SEND DATA\n";
-                chomp($result);
-                print "Result:\[$result\]\n";
-            }
-
-            DebugErrors();
-        }
-        else
-        {
-            print "Successfully sent status:\[$state\] for branch:\[$branch\]\n";
-        }
-    }
+  }
 }
 #######################################################################
 #### SUB ROUTINE DebugErrors ##########################################
 sub DebugErrors
 {
-    print "###########################\n";
-    print "#### DEBUG ERRORS LOOP ####\n";
-    print "##### STEP HAS FAILED #####\n";
-    print "#### END TRANSMISSION ####\n";
-    print "###### EXITING NOW #######\n";
-    print "##########################\n";
+  print "###########################\n";
+  print "#### DEBUG ERRORS LOOP ####\n";
+  print "##### STEP HAS FAILED #####\n";
+  print "#### END TRANSMISSION ####\n";
+  print "###### EXITING NOW #######\n";
+  print "##########################\n";
 
-    exit(1);
+  exit(1);
 }
